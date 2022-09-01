fb8228e329ff1b0d6f570704e7d46bfbb3b95877
stgom <s.moya13@alumnos.santotomas.cl>
2022-09-01 12:07:53 -0400

    ovo

--- app/src/main/java/com/example/miprimerapp/MainActivity.java
+++ app/src/main/java/com/example/miprimerapp/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.miprimerapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private CheckBox seleccionarChk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        seleccionarChk = (CheckBox) findViewById(R.id.checkBox);
+    }
+
+    public void btnClick(View v){
+        if(seleccionarChk.isChecked() == true){
+            String mensaje = "Seleccionado";
+            Toast toast = Toast.makeText(this, mensaje, Toast.LENGTH_LONG);
+            toast.show();
+        }else{
+            String mensaje = "No Seleccionado";
+            Toast toast = Toast.makeText(this, mensaje, Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.START, 90, 0);
+            toast.show();
+        }
+
     }
 }
